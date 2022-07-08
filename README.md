c7da3ac86a90f476552e97a0a31dea2e5f134cab
aitor <penguinft1@gmail.com>
2022-07-08 19:53:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Basic Tinder built in React
+
+![Preview](tinder-preview.png?raw=true)
---
+++ src/components/actions/Rewind.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import { useNavigate } from 'react-router-dom';
+
+export default function Rewind () {
+  
+  return (
+  
+  <button type="button">
+    <img src="images/misc/rewind.png" alt="Rewind User" />
+  </button>
+
+)}
+
+
+
+/* TO DO
+TRYING TO NAVIGATE TO PREVIOUS STEP WITHOUT THE USE OF ROUTER
+const navigate = useNavigate();
+onClick={() => navigate(-1)} */
